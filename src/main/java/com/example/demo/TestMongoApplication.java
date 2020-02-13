32dab55b399c699006f2ed1b3f581ad57ac3909b
khanhnguyenba <khanhnb3@topica.edu.vn>
2020-02-13 15:05:51 +0700

    sample mongodb spring boot

---
+++ src/main/java/com/example/demo/TestMongoApplication.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import com.example.demo.entity.Customer;
+import com.example.demo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMongoApplication implements CommandLineRunner {
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMongoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        customerRepository.deleteAll();
+
+        customerRepository.save(new Customer("Alice", "Smith"));
+        customerRepository.save(new Customer("Bob", "Smith"));
+
+        // fetch all customers
+        System.out.println("Customers found with findAll():");
+        System.out.println("-------------------------------");
+        for (Customer customer : customerRepository.findAll()) {
+            System.out.println(customer);
+        }
+
+        System.out.println();
+
+        // fetch an individual customer
+        System.out.println("Customer found with findByFirstName('Alice'):");
+        System.out.println("--------------------------------");
+        System.out.println(customerRepository.findByFirstName("Alice"));
+
+        System.out.println("Customers found with findByLastName('Smith'):");
+        System.out.println("--------------------------------");
+        for (Customer customer : customerRepository.findByLastName("Smith")) {
+            System.out.println(customer);
+        }
+
+    }
+}
